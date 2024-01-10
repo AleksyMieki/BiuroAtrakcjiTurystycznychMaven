34abe2187ba13646c303850d9dd79a2282301f97
Aleksy Mieki <alemie183_dpp_2023@e-science.pl>
2024-01-10 16:42:53 +0100

    dodalem wstepny test

--- BiuroAtrakcjiMaven/src/test/java/Aplikacja/AplikacjaTest.java
+++ BiuroAtrakcjiMaven/src/test/java/Aplikacja/AplikacjaTest.java
@@ -1,7 +1,39 @@
 package Aplikacja;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.TestExecutionExceptionHandler;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class AplikacjaTest {
+@Tag("Entity")
+@Tag("Control")
+class AplikacjaTest implements TestExecutionExceptionHandler {
 
+    Dane dane = new Dane("1",1,"1","1");
+
+    @BeforeEach
+    public void setUp()
+    {
+        Atrakcja atrakcja = new Atrakcja(dane);
+    }
+
+    @Test
+    @ExtendWith(AplikacjaTest.class)
+    public void testDodajAtrakcje()
+    {
+        Aplikacja aplikacja = Aplikacja.getInstance();
+
+        assertEquals(0,0);
+
+    }
+
+
+    @Override
+    public void handleTestExecutionException(ExtensionContext extensionContext, Throwable throwable) throws Throwable {
+
+    }
 }
