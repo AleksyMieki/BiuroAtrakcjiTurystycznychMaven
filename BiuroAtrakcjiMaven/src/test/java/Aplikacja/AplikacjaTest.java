aa06a98dd26d7caee97e8ea17813140fdb227e96
Piotr Kuchnowski <kuchnowski.piotr@outlook.com>
2024-01-16 14:18:13 +0100

    AplikacjaTest

--- BiuroAtrakcjiMaven/src/test/java/Aplikacja/AplikacjaTest.java
+++ BiuroAtrakcjiMaven/src/test/java/Aplikacja/AplikacjaTest.java
@@ -1,39 +1,30 @@
 package Aplikacja;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestExecutionExceptionHandler;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@Tag("Entity")
-@Tag("Control")
 class AplikacjaTest implements TestExecutionExceptionHandler {
+    static Aplikacja instance;
 
-    Dane dane = new Dane("1",1,"1","1");
+    @Override
+    public void handleTestExecutionException(ExtensionContext context, Throwable throwable)
+            throws Throwable {
+        if(throwable instanceof NullPointerException) {
+            System.out.println("Wprowadzone dane są nieprawidłowe, unikaj wartości NULL");
+        }
+        else{ throw throwable; }}
 
-    @BeforeEach
-    public void setUp()
-    {
-        Atrakcja atrakcja = new Atrakcja(dane);
+    @BeforeAll
+    static void setUp() {
+        instance = Aplikacja.getInstance();
     }
 
     @Test
-    @ExtendWith(AplikacjaTest.class)
-    public void testDodajAtrakcje()
-    {
-        Aplikacja aplikacja = Aplikacja.getInstance();
-
-        assertEquals(0,0);
-
-    }
-
-
-    @Override
-    public void handleTestExecutionException(ExtensionContext extensionContext, Throwable throwable) throws Throwable {
-
+    void utworzZgloszenie() {
     }
 }
